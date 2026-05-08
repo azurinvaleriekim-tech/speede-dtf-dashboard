e0c0ec98123315a5ca686b24f2bddcecf194e737
azurinvaleriekim-tech <azurinvaleriekim@gmail.com>
2026-05-09 03:04:39 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { DashboardView } from "@/components/DashboardView";
+import { DashboardView } from "../components/DashboardView";
 
 export default function HomePage() {
   return <DashboardView />;
