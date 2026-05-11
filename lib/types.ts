661dd45e37caee63cf0cb2cf56342424df24543b
azurinvaleriekim-tech <azurinvaleriekim@gmail.com>
2026-05-11 23:59:44 +0800

    Update types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -1,7 +1,8 @@
 export type DashboardSettings = {
   darkMode?: boolean;
   cutoffTime?: string;
-  afterHoursTime?: string;
+ afterHoursTime?: string;
+  refreshIntervalSeconds?: number;
 };
 
 export type OrderStatus =
