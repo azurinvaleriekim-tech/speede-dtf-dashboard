96b7f6126cac0f578559fb1a0b9debcead9d2cc2
azurinvaleriekim-tech <azurinvaleriekim@gmail.com>
2026-05-12 01:14:19 +0800

    Update time.ts

--- lib/time.ts
+++ lib/time.ts
@@ -1,3 +1,14 @@
 export function formatTime(date: string | number | Date) {
   return new Date(date).toLocaleTimeString();
 }
+export function formatDurationFrom(date: string | number | Date) {
+  const diff = Math.floor(
+    (Date.now() - new Date(date).getTime()) / 1000
+  );
+
+  const minutes = Math.floor(diff / 60);
+  const hours = Math.floor(minutes / 60);
+
+  if (hours > 0) return `${hours}h ${minutes % 60}m`;
+  return `${minutes}m`;
+}
