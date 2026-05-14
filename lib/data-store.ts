1413e1dffcdc026c9b98e4dde79cd73e96195228
azurinvaleriekim-tech <azurinvaleriekim@gmail.com>
2026-05-15 01:08:58 +0800

    Update data-store.ts

--- lib/data-store.ts
+++ lib/data-store.ts
@@ -6,7 +6,7 @@ const consumerSecret = process.env.WOOCOMMERCE_CONSUMER_SECRET!;
 
 async function fetchOrders() {
   const response = await fetch(
-    `${baseUrl}/wp-json/wc/v3/orders?per_page=250`,
+    `${baseUrl}/wp-json/wc/v3/orders?per_page=100`,
     {
       headers: {
         Authorization:
